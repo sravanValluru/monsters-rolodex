f6d1dd3912a5520e28941a8ba4d55a5ac297edfc
sravanValluru <sravantinkumar@gmail.com>
2019-10-29 21:21:23 +0530

    Updates

---
+++ precache-manifest.f7c78834ac5070d4767bc66b49b63cad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb22b9690600b086b3ca3dedba8cca7d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f943f57172e4cfdbc883",
+    "url": "/monsters-rolodex/static/css/main.1497fb73.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "f943f57172e4cfdbc883",
+    "url": "/monsters-rolodex/static/js/main.e697c3d4.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
